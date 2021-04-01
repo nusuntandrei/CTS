713d500f579f5e872fb5539871a787e9b2faf2e7
ANDREI <gheorghisor.andrei99@yahoo.com>
2021-04-01 15:20:11 +0300

    Final Commit

--- Week6 - Prototype Builder/src/ro/ase/csie/cts/g1093/dp/builder/TestBuilder.java
+++ Week6 - Prototype Builder/src/ro/ase/csie/cts/g1093/dp/builder/TestBuilder.java
@@ -3,9 +3,22 @@ package ro.ase.csie.cts.g1093.dp.builder;
 public class TestBuilder {
 
 	public static void main(String[] args) {
+			
+			//1. create the object
+			//SuperHero superHero = new SuperHero();
+			//2.Init it
+			//TODO: don't forget
+			//SuperHero superHero2 = new SuperHero("Superman", 100, false, false, new Weapon(), null, new Flying(), null);
 		
-			SuperHero superHero = new SuperHero();
-			SuperHero superHero2 = new SuperHero("Superman", 100, false, false, new Weapon(), null, new Flying(), null);
+		SuperHero superman = 
+				new SuperHero.SuperHeroBuilder("Superman", 100)
+				.build();
+		SuperHero joker = 
+				new SuperHero.SuperHeroBuilder("Joker", 200).
+				isVillain().
+				isWounded().
+				setRightWeapon(new Weapon()).
+				build();
 	}
 
 }
