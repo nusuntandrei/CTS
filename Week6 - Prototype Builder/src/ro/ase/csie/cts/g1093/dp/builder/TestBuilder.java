498ab79faf9ba49f1d524ec85eb4abd0081adf13
ANDREI <gheorghisor.andrei99@yahoo.com>
2021-04-01 14:48:58 +0300

    Half Time

---
+++ Week6 - Prototype Builder/src/ro/ase/csie/cts/g1093/dp/builder/TestBuilder.java
@@ -0,0 +1,11 @@
+package ro.ase.csie.cts.g1093.dp.builder;
+
+public class TestBuilder {
+
+	public static void main(String[] args) {
+		
+			SuperHero superHero = new SuperHero();
+			SuperHero superHero2 = new SuperHero("Superman", 100, false, false, new Weapon(), null, new Flying(), null);
+	}
+
+}
---
+++ Week6 - Prototype Builder/src/ro/ase/csie/cts/g1093/dp/builder/Weapon.java
@@ -0,0 +1,12 @@
+package ro.ase.csie.cts.g1093.dp.builder;
+
+public class Weapon implements WeaponInterface {
+
+	@Override
+	public void pewPew() {
+		System.out.println("-> -> -> -> -> pew pew");
+	}
+	
+	
+}
+
