49d5e9a791ac9d5b965879de72525543347b1489
nusuntandrei <gheorghisor.andrei99@yahoo.com>
2021-05-13 15:25:46 +0300

    Implementing Command

---
+++ Week11 - Command & Chain/src/ro/ase/csie/cts/g1093/dp/command/AutoSaveModule.java
@@ -0,0 +1,18 @@
+package ro.ase.csie.cts.g1093.dp.command;
+
+public class AutoSaveModule implements GameModuleInterface{
+
+	
+	int frequency;
+	
+	public AutoSaveModule(int frequency) {
+		super();
+		this.frequency = frequency;
+	}
+	
+	@Override
+	public void doTheTask(String details) {
+		// TODO Auto-generated method stub
+		
+	}
+}
