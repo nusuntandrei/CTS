a4f00505b1c91c0a308d89aae10f3623dfe3e116
nusuntandrei <gheorghisor.andrei99@yahoo.com>
2021-05-27 15:30:37 +0300

    Unit Testing

--- Week13 - Unit Testing/src/ro/ase/csie/cts/g1093/testing/tests/TestStudent.java
+++ Week13 - Unit Testing/src/ro/ase/csie/cts/g1093/testing/tests/TestStudent.java
@@ -12,6 +12,7 @@ import org.junit.Ignore;
 import org.junit.Test;
 
 import ro.ase.csie.cts.g1093.testing.exceptions.WrongAgeException;
+import ro.ase.csie.cts.g1093.testing.exceptions.WrongGradeException;
 import ro.ase.csie.cts.g1093.testing.exceptions.WrongNameException;
 import ro.ase.csie.cts.g1093.testing.models.Student;
 
@@ -86,5 +87,57 @@ public class TestStudent {
 	public void testSetNameErrorConditionSmallName() throws WrongNameException {
 		String newName = "Io";
 		student.setName(newName);
+	}
+	
+	@Test
+	public void testGetGradesAverageOrderingAsc() throws WrongGradeException {
+		ArrayList<Integer> grades = new ArrayList<>();
+		for(int i = 6; i < 10; i++) {
+			grades.add(i);
+		}
+		
+//		grades.add(6);
+//		grades.add(6);
+//		grades.add(7);
+		
+		student.setGrades(grades);
+		
+		float expectedAverage = 7.5f;
+		float computedAverage = student.getGradesAverage();
+		
+		assertEquals("Testing with sorted array of grades", expectedAverage, computedAverage, 0);
+	}
+	
+	@Test
+	public void testGetGradesAverageCardinalityZero() throws WrongGradeException {
+		ArrayList<Integer> grades = new ArrayList<>();
+		student.setGrades(grades);
+		
+		float expectedAverage = 0;
+		float computedAverage = student.getGradesAverage();
+		
+		assertEquals("Testing with sorted array of grades", expectedAverage, computedAverage, 0);
+	}
+	
+	@Test
+	public void testGetGradesAverageCardinalityOne() throws WrongGradeException {
+		ArrayList<Integer> grades = new ArrayList<>();
+		grades.add(Student.MAX_GRADE);
+		student.setGrades(grades);
+		
+		float expectedAverage = Student.MAX_GRADE;
+		float computedAverage = student.getGradesAverage();
+		
+		assertEquals("Testing with sorted array of grades", expectedAverage, computedAverage, 0);
+	}
+	
+	@Test
+	public void testGetGradesAverageExistanceNullRefferenceForGrades() throws WrongGradeException {
+		student.setGrades(null);
+		
+		float expectedAverage = 0;
+		float computedAverage = student.getGradesAverage();
+		
+		assertEquals("Testing with null for grades", expectedAverage, computedAverage, 0);
 	}
 }
